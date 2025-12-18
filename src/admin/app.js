428a485dcb5b669b2d08577dfe257e8efc17636e
Lorenzo Galassi <lorenzo.galassi91@gmail.com>
2025-12-18 10:34:04 +0100

    feat: add new translations

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,32 +1,22 @@
 const config = {
-  locales: [
-    // 'ar',
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    // 'es',
-    // 'he',
-    // 'id',
-    'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    // 'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
+  locales: ["it"],
+  translations: {
+    it: {
+      "Auth.form.welcome.title": "Benvenuto sull'intranet del Comune di Rivoli",
+      "Auth.form.welcome.subtitle":
+        "Collegati inserendo la tua mail e la tua password",
+      "HomePage.header.title": "Buongiorno {name}",
+      "HomePage.header.subtitle":
+        "Benvenuto nel tuo pannello di amministrazione",
+
+      "global.back": "Indietro",
+      "app.utils.published": "Pubblicato",
+      "app.components.ToggleCheckbox.off-label": "Falso",
+      "app.components.ToggleCheckbox.on-label": "Vero",
+
+      "components.RelationInput.addOrCreate": "My custom label",
+    },
+  },
 };
 
 const bootstrap = (app) => {
